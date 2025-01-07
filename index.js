227b754d4395ca532f32c36d8b76193f194f7f53
bekatur32 <103990593+bekatur32@users.noreply.github.com>
2025-01-07 14:37:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let button = document.getElementById("open")
+let closed = document.getElementById("closed")
+let block = document.getElementById("block")
+
+button.onclick = function(){
+    block.style.display = 'block'
+}
